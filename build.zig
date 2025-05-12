eba5d40616bf7dacd02ce2cc7819db709f7bc39c
Tuomas Laulaja <tuomas.laulaja@proton.me>
2025-05-12 21:20:01 +0300

    base on tinywl

--- build.zig
+++ build.zig
@@ -1,25 +1,56 @@
 // See LICENSE file for copyright and license details.
 const std = @import("std");
+const Scanner = @import("wayland").Scanner;
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
+    const scanner = Scanner.create(b, .{});
+    scanner.addSystemProtocol("stable/xdg-shell/xdg-shell.xml");
+    scanner.addSystemProtocol("stable/tablet/tablet-v2.xml");
+
+    scanner.generate("wl_compositor", 4);
+    scanner.generate("wl_subcompositor", 1);
+    scanner.generate("wl_shm", 1);
+    scanner.generate("wl_output", 4);
+    scanner.generate("wl_seat", 7);
+    scanner.generate("wl_data_device_manager", 3);
+    scanner.generate("xdg_wm_base", 2);
+    scanner.generate("zwp_tablet_manager_v2", 1);
+
+    const wayland = b.createModule(.{ .root_source_file = scanner.result });
+    const xkbcommon = b.dependency("xkbcommon", .{}).module("xkbcommon");
+    const pixman = b.dependency("pixman", .{}).module("pixman");
+    const wlroots = b.dependency("wlroots", .{}).module("wlroots");
+
+    wlroots.addImport("wayland", wayland);
+    wlroots.addImport("xkbcommon", xkbcommon);
+    wlroots.addImport("pixman", pixman);
+
+    wlroots.resolved_target = target;
+    wlroots.linkSystemLibrary("wlroots-0.18", .{});
+
+    const ghostwm = b.addExecutable(.{
         .root_source_file = b.path("src/main.zig"),
+        .name = "ghostwm",
         .target = target,
         .optimize = optimize,
     });
 
-    const exe = b.addExecutable(.{
-        .name = "ghostwm",
-        .root_module = exe_mod,
-    });
+    ghostwm.linkLibC();
 
-    b.installArtifact(exe);
+    ghostwm.root_module.addImport("wayland", wayland);
+    ghostwm.root_module.addImport("xkbcommon", xkbcommon);
+    ghostwm.root_module.addImport("wlroots", wlroots);
 
-    const run_cmd = b.addRunArtifact(exe);
+    ghostwm.linkSystemLibrary("wayland-server");
+    ghostwm.linkSystemLibrary("xkbcommon");
+    ghostwm.linkSystemLibrary("pixman-1");
+
+    b.installArtifact(ghostwm);
+
+    const run_cmd = b.addRunArtifact(ghostwm);
     run_cmd.step.dependOn(b.getInstallStep());
 
     if (b.args) |args| {
--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,29 @@
 // See LICENSE file for copyright and license details.
 const std = @import("std");
+const posix = std.posix;
+const Server = @import("wayland.zig").Server;
+const gpa = std.heap.c_allocator;
 
-pub fn main() !void {
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+pub fn main() anyerror!void {
+    var server: Server = undefined;
+    try server.init();
+    defer server.deinit();
 
-    try stdout.print("Hello, World!\n", .{});
-    try bw.flush();
+    var buf: [11]u8 = undefined;
+    const socket = try server.wl_server.addSocketAuto(&buf);
+
+    if (std.os.argv.len >= 2) {
+        const cmd = std.mem.span(std.os.argv[1]);
+        var child = std.process.Child.init(&[_][]const u8{ "/bin/sh", "-c", cmd }, gpa);
+        var env_map = try std.process.getEnvMap(gpa);
+        defer env_map.deinit();
+        try env_map.put("WAYLAND_DISPLAY", socket);
+        child.env_map = &env_map;
+        try child.spawn();
+    }
+
+    try server.backend.start();
+
+    std.log.info("Running compositor on WAYLAND_DISPLAY={s}", .{socket});
+    server.wl_server.run();
 }
-
